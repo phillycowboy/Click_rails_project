323ed3ad957d51bb2677b5c93c6cd100f5853a45
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-01 14:29:52 -0500

    migrated tables

--- db/migrate/20210201172700_create_photos.rb
+++ db/migrate/20210201172700_create_photos.rb
@@ -1,7 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[6.1]
   def change
     create_table :photos do |t|
-
+      t.string :image 
+      t.string :description 
       t.timestamps
     end
   end
--- db/migrate/20210201172905_create_comments.rb
+++ db/migrate/20210201172905_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-
+      t.string :content 
+      t.integer :user_id 
+      t.integer :photo_id 
       t.timestamps
     end
   end
