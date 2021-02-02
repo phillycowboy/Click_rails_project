a4e78e1b3ac30daa252ffc1fdebd3a353369f8ff
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-02 14:13:34 -0500

    can create new users

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bcrypt', '~> 3.1.7'
 gem 'yarn', '~> 0.1.1'
 gem 'optimist', '~> 3.0'
+gem 'pry'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get '/signup' => 'users#new'
+  # get '/signup' => 'users#new'
+  # post '/signup' => 'users#show'
 
 
+  resources :users
   resources :comments
   resources :photos
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
