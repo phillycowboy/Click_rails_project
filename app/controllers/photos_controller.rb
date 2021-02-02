7e9ed343ded06588a9e2dfed56dd8cb73ce6fdcc
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-02 17:15:44 -0500

    added new photo form, and index view after the user creates new object.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,27 @@
 class PhotosController < ApplicationController
+
+    before_action :redirect_if_not_logged_in
+
+    def index 
+        @photos = Photo.all 
+    end
+
+    def new 
+        @photo = Photo.new 
+    end
+
+    def create 
+        @photo = Photo.new(photo_params)
+        if @photo.save 
+            redirect_to photos_path
+        else 
+            render :new
+        end
+    end
+
+    private 
+
+    def photo_params 
+        params.require(:photo).permit(:image, :description)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
             session[:user_id] = user.id 
             redirect_to user_path(user)
         else
-            flash[:message] = "User does not exist, please Sign Up."
+            flash[:message] = "Incorrect Login Info, please try again."
             redirect_to login_path 
         end
     end
