7a0494aefe790f8a6816fda56e100562b579e40f
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-02 15:10:23 -0500

    added redirect if user does not exist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,14 @@
 class SessionsController < ApplicationController
 
+
+    def new
+
+    end
+
+    def create
+         
+    end
+
     def destroy 
         session.delete :user_id
         redirect_to root_path 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     
     def show 
         @user = User.find_by(id: params[:id])
+        redirect_to root_path if !@user 
     end
 
     private 
