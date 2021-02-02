f2d29ff4a7ac2f383c9eca81fece50d55235c6c2
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-02 15:54:30 -0500

    added login credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-         
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id 
+            redirect_to user_path(user)
+        else
+            flash[:message] = "User does not exist, please Sign Up."
+            redirect_to login_path 
+        end
     end
 
     def destroy 
