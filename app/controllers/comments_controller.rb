f55166466c9ae54d6aab8589a47618c7ad0c4ff8
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-03 16:08:01 -0500

    testing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,17 @@ class CommentsController < ApplicationController
     end
 
     def create 
-        
+        @comment = Comment.new(comment_params)
+        if @comment.save 
+            redirect_to photo_comments_path(params[:photo_id], @comment)
+        else 
+            render :new 
+        end
+    end
+
+    private 
+
+    def comment_params 
+        params.require(:comment).permit(:content, :user_id, :photo_id)
     end
 end
