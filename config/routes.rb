2ccee374d0b8cd69d62e248e8e0b30520fdb2fde
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-03 14:44:08 -0500

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   resources :users, except: [:new]
   get '/signup', to: "users#new", as: "signup"
-  resources :comments
   resources :photos
+  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
