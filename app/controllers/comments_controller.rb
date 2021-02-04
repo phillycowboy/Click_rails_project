607c0d0444610b5c040ec9f4198974709092768c
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-04 14:01:43 -0500

    added validations for photo and comment and added partials for forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,31 +1,32 @@
 class CommentsController < ApplicationController
-
+    before_action :find_photo 
     def index
 
     end
 
     def show 
-        @photo = Photo.find(params[:photo_id])
         @comment = Comment.find(params[:id])
     end
-    
+
     def new 
-        @photo = Photo.find_by(id: params[:photo_id])
         @comment = @photo.comments.build
     end
 
     def create 
-        @comment = Comment.new(comment_params)
+        @comment = @photo.comments.build(comment_params)
         @comment.user_id = session[:user_id]
-        @comment.photo_id = params[:photo_id]
         if @comment.save 
-            redirect_to photo_comment_path(params[:photo_id], @comment)
+            redirect_to photo_comment_path(@photo, @comment)
         else 
             render :new 
         end
     end
 
     private 
+
+    def find_photo 
+        @photo = Photo.find_by(id: params[:photo_id])
+    end
 
     def comment_params 
         params.require(:comment).permit(:content, :user_id, :photo_id)
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
     
     has_many :comments
     has_many :users, through: :comments
+
+    validates :image, presence: true 
 end
