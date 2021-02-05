b7b73ddeb4f8a0796d02e71ce3de0c2f82003903
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-05 11:41:19 -0500

    added validations for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def create
+    def create 
         @user = User.new(user_params)
         if @user.save 
             session[:user_id] = @user.id 
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
     def show 
         redirect_if_not_logged_in 
         @user = User.find_by(id: params[:id])
-        # redirect_to root_path if !@user 
     end
 
     private 
