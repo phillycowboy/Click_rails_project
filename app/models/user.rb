759376683276fcf22c5fbbddf66f7cdac0103218
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-05 11:47:26 -0500

    added secure validations for user user cannot be recreated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     has_many :photos, through: :comments 
     has_secure_password 
     validates :username, presence: true 
-    validates :password, length: {minimum: 6, message: "Password must have at least 6 characters."} 
+    validates :username, uniqueness: true 
+    validates :password, length: {minimum: 6, message: "must have at least 6 characters."} 
 end
