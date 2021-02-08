25361268a51353af89f9a1fdf2b89987375a5208
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-02-08 15:46:21 -0500

    users cannot edit/delete others photos or comments.  updated code to use before_action to locate objects.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,17 @@
 class CommentsController < ApplicationController
     before_action :redirect_if_not_logged_in
-    before_action :find_photo
+    before_action :owns_comment?, only: [:edit, :update, :destroy]
+    before_action :photo_find 
+    before_action :find_comment, only: [:show, :edit, :update, :destroy]
 
-    def index
-        
-    end
+
 
     def show 
-        @comment = Comment.find(params[:id])
+        # @comment = Comment.find(params[:id])
     end
 
     def new 
-        @comment = @photo.comments.build
+        @comment = @photo.comments.build 
     end
 
     def create 
@@ -25,12 +25,12 @@ class CommentsController < ApplicationController
     end
 
     def edit 
-        @photo = Photo.find_by(id: params[:photo_id])
-        @comment = Comment.find_by(id: params[:id])
+        # @photo = Photo.find_by(id: params[:photo_id])
+        # @comment = Comment.find_by(id: params[:id])
     end
 
     def update 
-        @comment = Comment.find_by(id: params[:id])
+        # @comment = Comment.find_by(id: params[:id])
         if @comment.update(comment_params)
             redirect_to photo_path(@comment.photo_id)
         else 
@@ -39,16 +39,32 @@ class CommentsController < ApplicationController
     end
 
     def destroy 
-        @comment = Comment.find_by(id: params[:id])
+        # @comment = Comment.find_by(id: params[:id])
         @comment.destroy 
         redirect_to photo_path(@comment.photo_id)
     end
 
     private 
-
-    def find_photo 
-        @photo = Photo.find_by(id: params[:photo_id])
+    def owns_comment?
+        # @comment = Comment.find_by(id: params[:id])
+        find_comment
+        if @comment.user != current_user
+            redirect_to photo_path(@comment.photo_id)
+            flash[:message] = "You do not have permission to edit this comment!"
+        end
     end
+    
+    def find_comment 
+        @comment = Comment.find_by(id: params[:id])
+    end
+
+    def photo_find 
+        @photo = Photo.find_by(id: params[:photo_id])
+     end
+
+    # def find_photo 
+    #     @photo = Photo.find_by(id: params[:photo_id])
+    # end
 
     def comment_params 
         params.require(:comment).permit(:content, :user_id, :photo_id, :hashtag_id)
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,15 @@
 class PhotosController < ApplicationController
 
     before_action :redirect_if_not_logged_in
-    before_action :photo_find, only: [:edit, :update, :destroy]
+    before_action :owns_photo?, only: [:edit, :update, :destroy]
+    before_action :photo_find, only: [:show, :edit, :update, :destroy]
 
     def index 
         @photos = Photo.search(params[:search]).reverse
     end
     
     def show 
-        @photo = Photo.find_by(id: params[:id])
+        # @photo = Photo.find_by(id: params[:id])
     end
     
     def new 
@@ -26,12 +27,12 @@ class PhotosController < ApplicationController
 
 
     def edit 
-        @photo = Photo.find_by(id: params[:id])
+        # @photo = Photo.find_by(id: params[:id])
         # @user = User.find_by(id: params[:id])
     end
 
     def update 
-        @photo = Photo.find_by(id: params[:id])
+        # @photo = Photo.find_by(id: params[:id])
         if @photo.update(photo_params)
             redirect_to photo_path(@photo)
         else 
@@ -40,20 +41,25 @@ class PhotosController < ApplicationController
     end
 
     def destroy 
-        @photo = Photo.find(params[:id])
+        # @photo = Photo.find(params[:id])
         @photo.destroy
         redirect_to photos_path
     end
 
     private 
 
-    def photo_find 
-        @photo = Photo.find_by(id: params[:id])
+    def owns_photo? 
+        # @photo = Photo.find_by(id: params[:id])
+        photo_find
         if @photo.user != current_user
             redirect_to photo_path(@photo) 
             flash[:message] = "You do not have permission to edit this photo!"
         end
     end
+
+    def photo_find 
+        @photo = Photo.find_by(id: params[:id])
+     end
 
     def photo_params 
         params.require(:photo).permit(:image, :description)
